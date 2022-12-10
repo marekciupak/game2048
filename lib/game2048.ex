3c5516f65d96a2874c72a60a8fc58b84555f79a0
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2022-12-10 12:57:33 +0100

    Bootstrap Elixir project
    ```
    $ mix new game2048
    ```

---
+++ lib/game2048.ex
@@ -0,0 +1,18 @@
+defmodule Game2048 do
+  @moduledoc """
+  Documentation for `Game2048`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Game2048.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/game2048_test.exs
@@ -0,0 +1,8 @@
+defmodule Game2048Test do
+  use ExUnit.Case
+  doctest Game2048
+
+  test "greets the world" do
+    assert Game2048.hello() == :world
+  end
+end
