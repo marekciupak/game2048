915b08ecfbb6e51020cd5b4b03a7937e3e6cf6a2
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2022-12-10 19:01:23 +0100

    Implement sliding left and right

--- lib/game2048.ex
+++ lib/game2048.ex
@@ -3,16 +3,33 @@ defmodule Game2048 do
   Documentation for `Game2048`.
   """
 
+  alias Game2048.Row
+
   @doc """
-  Hello world.
+  Slides tiles as far as possible in the given direction.
 
   ## Examples
 
-      iex> Game2048.hello()
-      :world
+      iex> Game2048.slide(
+      ...>   [
+      ...>     [:empty, 2, :empty],
+      ...>     [2, 2, 2],
+      ...>     [2, 8, 4]
+      ...>   ],
+      ...>   :left
+      ...> )
+      [
+        [2, :empty, :empty],
+        [4, 2, :empty],
+        [2, 8, 4]
+      ]
 
   """
-  def hello do
-    :world
+  def slide(grid, :left) do
+    Enum.map(grid, &Row.slide_left(&1))
+  end
+
+  def slide(grid, :right) do
+    Enum.map(grid, &Row.slide_right(&1))
   end
 end
---
+++ lib/game2048/row.ex
@@ -0,0 +1,77 @@
+defmodule Game2048.Row do
+  @moduledoc """
+  Documentation for `Game2048.Row`.
+  """
+
+  @doc """
+  Slides tiles as far as possible to the left. Tiles can slide through empty spots and can move other tiles until they
+  are stopped by the edge of the row.
+
+  If two tiles of the same number collide while moving, they will merge into a tile with the total value of the two
+  tiles that collided. The resulting tile cannot merge with another tile again.
+
+  ## Examples
+
+      iex> Game2048.Row.slide_left([:empty, 1, 2, :empty, 4])
+      [1, 2, 4, :empty, :empty]
+
+      iex> Game2048.Row.slide_left([2, 2, :empty, 4, 4])
+      [4, 8, :empty, :empty, :empty]
+
+      iex> Game2048.Row.slide_left([8, 8, 8])
+      [16, 8, :empty]
+
+  """
+  def slide_left(row) do
+    row
+    |> slide()
+    |> Enum.reverse()
+  end
+
+  @doc """
+  Slides tiles as far as possible to the right.
+
+  All the rules are the same as when sliding to the left (only the direction is different) - take a look at
+  `Game2048.Row.slide_left/1` for details.
+
+  ## Examples
+
+      iex> Game2048.Row.slide_right([:empty, 1, 2, :empty, 4])
+      [:empty, :empty, 1, 2, 4]
+
+      iex> Game2048.Row.slide_right([2, 2, :empty, 4, 4])
+      [:empty, :empty, :empty, 4, 8]
+
+      iex> Game2048.Row.slide_right([8, 8, 8])
+      [:empty, 8, 16]
+
+  """
+  def slide_right(row) do
+    row
+    |> Enum.reverse()
+    |> slide()
+  end
+
+  defp slide(_row, acc \\ [], empty_spots \\ [])
+
+  defp slide([:empty | rest], acc, empty_spots) do
+    slide(rest, acc, [:empty | empty_spots])
+  end
+
+  defp slide([tile, :empty | rest], acc, empty_spots) do
+    slide([tile | rest], acc, [:empty | empty_spots])
+  end
+
+  defp slide([tile, tile | rest], acc, empty_spots) do
+    merged_tile = 2 * tile
+    slide(rest, [merged_tile | acc], [:empty | empty_spots])
+  end
+
+  defp slide([tile | rest], acc, empty_spots) do
+    slide(rest, [tile | acc], empty_spots)
+  end
+
+  defp slide([], acc, empty_spots) do
+    empty_spots ++ acc
+  end
+end
