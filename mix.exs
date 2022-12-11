6d0b4260cf17e0b24dd6927fc44245c1192a3ebf
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2022-12-11 20:25:53 +0100

    Add ecto

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Game2048.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.15"},
+      {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
