8351113c13152c2f7a0a8c610ef0a3ca77faf299
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2022-12-12 20:03:42 +0100

    Add logic for adding a new tile on every move

--- lib/game2048/grid.ex
+++ lib/game2048/grid.ex
@@ -25,6 +25,8 @@ defmodule Game2048.Grid do
   @type spot_position :: {spot_position_x, spot_position_y}
   @type direction :: :left | :right | :up | :down
 
+  @tile_that_appears_on_every_move 1
+
   @doc """
   Generates a new grid.
 
@@ -34,7 +36,7 @@ defmodule Game2048.Grid do
 
     iex> Game2048.Grid.generate(size: {3, 2}, number_of_obstacles: 1, starting_tiles: [4, 8])
     [
-      [4, 8, :empty],
+      [4, :empty, 8],
       [:empty, :obstacle, :empty]
     ]
   """
@@ -55,150 +57,21 @@ defmodule Game2048.Grid do
   end
 
   @doc """
-  ## Examples
-
-      iex> Game2048.Grid.place_on_random_empty_spots([
-      ...>   [1, 2, 3],
-      ...>   [4, 5, :empty],
-      ...>   [6, 7, 8]
-      ...> ], [:obstacle])
-      [
-        [1, 2, 3],
-        [4, 5, :obstacle],
-        [6, 7, 8]
-      ]
-
-  """
-  @spec place_on_random_empty_spots(t, list(Row.element())) :: t
-  defp place_on_random_empty_spots(grid, elements) do
-    number_of_elements = Enum.count(elements)
-
-    grid
-    |> empty_spots_positions()
-    |> ensure_enough_spots!(number_of_elements)
-    |> Enum.take_random(number_of_elements)
-    |> assign_elements_to_positions(elements)
-    |> place_on_spots(grid)
-  end
-
-  @doc """
-  ## Examples
-
-      iex> Game2048.Grid.empty_spots_positions([
-      ...>   [1, 2, :empty],
-      ...>   [:empty, :empty, :empty],
-      ...>   [3, 4, 5]
-      ...> ])
-      ...> |> Enum.sort()
-      [
-        {0, 1},
-        {1, 1},
-        {2, 0},
-        {2, 1}
-      ]
-
-  """
-  @spec empty_spots_positions(t) :: list(spot_position)
-  defp empty_spots_positions(grid) do
-    grid
-    |> Enum.with_index()
-    |> Enum.reduce([], fn {row, y}, acc ->
-      empty_spots_positions(row, y, acc)
-    end)
-  end
-
-  @spec empty_spots_positions(Row.t(), spot_position_y, list(spot_position)) :: list(spot_position)
-  defp empty_spots_positions(row, y, acc) do
-    row
-    |> Enum.with_index()
-    |> Enum.reduce(acc, fn {spot, x}, acc ->
-      case spot do
-        :empty -> [{x, y} | acc]
-        _ -> acc
-      end
-    end)
-  end
-
-  # TODO: Consider implementing a more advanced error handling and return errors instead of raising exceptions.
-  @spec ensure_enough_spots!(list(spot_position), pos_integer()) :: list(spot_position)
-  defp ensure_enough_spots!(spots_positions, minimal_number) do
-    if(Enum.count(spots_positions) < minimal_number, do: raise(NotEnoughSpotsError))
-
-    spots_positions
-  end
-
-  @doc """
-  ## Examples
-
-  iex> Game2048.Grid.assign_elements_to_positions(
-  ...>   [
-  ...>     {0, 1},
-  ...>     {1, 1},
-  ...>     {2, 0},
-  ...>     {2, 1}
-  ...>   ],
-  ...>   [2, 4, 8, :obstacle]
-  ...> )
-  %{
-    {0, 1} => 2,
-    {1, 1} => 4,
-    {2, 0} => 8,
-    {2, 1} => :obstacle
-  }
-
-  """
-  @spec assign_elements_to_positions(list(spot_position), list(Row.element())) :: %{spot_position => Row.element()}
-  defp assign_elements_to_positions(positions, elements) do
-    Enum.zip_reduce(positions, elements, %{}, &Map.put(&3, &1, &2))
-  end
-
-  @doc """
-  ## Examples
-
-  iex> Game2048.Grid.place_on_spots(
-  ...>   %{
-  ...>     {0, 0} => :obstacle,
-  ...>     {0, 1} => 4
-  ...>   },
-  ...>   [
-  ...>     [:empty, :empty],
-  ...>     [:empty, :empty]
-  ...>   ]
-  ...> )
-  [
-    [:obstacle, :empty],
-    [4, :empty]
-  ]
-
-  """
-  @spec place_on_spots(%{spot_position => Row.element()}, t) :: t
-  defp place_on_spots(positions_and_elements, grid) do
-    Enum.with_index(grid, fn row, y ->
-      Enum.with_index(row, fn spot, x ->
-        place_on_spot(positions_and_elements, {x, y}, spot)
-      end)
-    end)
-  end
-
-  @spec place_on_spot(%{spot_position => Row.element()}, spot_position, Row.spot()) :: t
-  defp place_on_spot(positions_and_elements, position, spot) do
-    case Map.fetch(positions_and_elements, position) do
-      {:ok, fetched_element} -> fetched_element
-      :error -> spot
-    end
-  end
-
-  @doc """
   Makes the player's move on the grid.
 
-  Player can move the gird in one of the four directions (`:left`, `:right`, `:up` or `:down`).
+  The movement consists of two steps:
 
-  The movement causes sliding all the tiles in the grid to a given direction.
+  1. Sliding all the tiles in the gird in direction selected by a player (`:left`, `:right`, `:up` or `:down`).
 
   The rules for sliding tiles in a single row are described in `Game2048.Row.slide_left/2` and
   `Game2048.Row.slide_right/2`. The rules for sliding the tiles in columns are exactly the same, apart from the
   difference in directions. The rules will be applied to each individual row (directions `:left` or `:right`) or column
   (directions `:up` or `:down`).
+
+  2. A new tile with number 1 appears in a random empty spot on the grid.
+
+  It may happen that a given move is illegal (a slide in a given direction does not cause any changes on the grid) -
+  in this case the function returns the grid without any changes.
 
   ## Examples
 
@@ -211,7 +84,7 @@ defmodule Game2048.Grid do
     ...>  :left
     ...>)
     [
-      [2, :empty, :empty],
+      [2, :empty, 1],
       [4, :empty, :empty],
       [2, 4, :empty]
     ]
@@ -226,7 +99,7 @@ defmodule Game2048.Grid do
     ...> )
     [
       [:empty, :empty, :empty],
-      [:empty, :obsctale, 4],
+      [1, :obsctale, 4],
       [2, 2, 4]
     ]
 
@@ -235,6 +108,7 @@ defmodule Game2048.Grid do
   def move(grid, direction) do
     grid
     |> slide(direction)
+    |> add_new_tile_if_grid_has_changed(prev_grid: grid)
   end
 
   @spec slide(t, direction) :: t
@@ -268,5 +142,86 @@ defmodule Game2048.Grid do
     grid
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
+  end
+
+  @spec add_new_tile_if_grid_has_changed(t, prev_grid: t) :: t
+  defp add_new_tile_if_grid_has_changed(current_grid, prev_grid: prev_grid) do
+    if current_grid != prev_grid do
+      place_on_random_empty_spots(current_grid, [@tile_that_appears_on_every_move])
+    else
+      current_grid
+    end
+  end
+
+  @spec place_on_random_empty_spots(t, list(Row.element())) :: t
+  defp place_on_random_empty_spots(grid, elements) do
+    number_of_elements = Enum.count(elements)
+
+    grid
+    |> empty_spots_positions()
+    |> ensure_enough_spots!(number_of_elements)
+    |> Enum.take_random(number_of_elements)
+    |> assign_elements_to_positions(elements)
+    |> place_on_spots(grid)
+  end
+
+  @spec empty_spots_positions(t) :: list(spot_position)
+  defp empty_spots_positions(grid) do
+    grid
+    |> Enum.with_index()
+    |> Enum.reduce([], fn {row, y}, acc ->
+      empty_spots_positions(row, y, acc)
+    end)
+  end
+
+  @spec empty_spots_positions(Row.t(), spot_position_y, list(spot_position)) :: list(spot_position)
+  defp empty_spots_positions(row, y, acc) do
+    row
+    |> Enum.with_index()
+    |> Enum.reduce(acc, fn {spot, x}, acc ->
+      case spot do
+        :empty -> [{x, y} | acc]
+        _ -> acc
+      end
+    end)
+  end
+
+  # TODO: Consider implementing a more advanced error handling and return errors instead of raising exceptions.
+  @spec ensure_enough_spots!(list(spot_position), pos_integer()) :: list(spot_position)
+  defp ensure_enough_spots!(spots_positions, minimal_number) do
+    if(Enum.count(spots_positions) < minimal_number, do: raise(NotEnoughSpotsError))
+
+    spots_positions
+  end
+
+  @spec assign_elements_to_positions(list(spot_position), list(Row.element())) :: %{spot_position => Row.element()}
+  defp assign_elements_to_positions(positions, elements) do
+    Enum.zip_reduce(positions, elements, %{}, &Map.put(&3, &1, &2))
+  end
+
+  @spec place_on_spots(%{spot_position => Row.element()}, t) :: t
+  defp place_on_spots(positions_and_elements, grid) do
+    Enum.with_index(grid, fn row, y ->
+      Enum.with_index(row, fn spot, x ->
+        place_on_spot(positions_and_elements, {x, y}, spot)
+      end)
+    end)
+  end
+
+  @spec place_on_spot(%{spot_position => Row.element()}, spot_position, Row.spot()) :: t
+  defp place_on_spot(positions_and_elements, position, spot) do
+    case Map.fetch(positions_and_elements, position) do
+      {:ok, fetched_element} -> fetched_element
+      :error -> spot
+    end
+  end
+
+  @doc """
+  Returns a tile with the number that appears on a grid on every player's move.
+
+  """
+  @spec tile_that_appears_on_every_move :: Row.tile()
+  def tile_that_appears_on_every_move do
+    @tile_that_appears_on_every_move
   end
 end
--- test/game2048/grid_test.exs
+++ test/game2048/grid_test.exs
@@ -1,6 +1,7 @@
 defmodule Game2048.GridTest do
   use ExUnit.Case, async: true
-  doctest Game2048.Grid, except: [generate: 1]
+
+  import Game2048.Grid, only: [tile_that_appears_on_every_move: 0]
 
   describe "Game2048.Grid.generate/1" do
     test "generates empty grid" do
@@ -43,72 +44,116 @@ defmodule Game2048.GridTest do
   end
 
   describe "Game2048.Grid.move/2" do
+    def reject_elements(grid, element) do
+      Enum.map(grid, fn row ->
+        Enum.map(row, fn spot ->
+          if spot == element do
+            :empty
+          else
+            spot
+          end
+        end)
+      end)
+    end
+
     test "slides tiles to the left" do
-      assert(
-        Game2048.Grid.move(
-          [
-            [2, :empty, 2],
-            [16, :obstacle, 16],
-            [:empty, 2, 32]
-          ],
-          :left
-        ) == [
-          [4, :empty, :empty],
-          [16, :obstacle, 16],
-          [2, 32, :empty]
-        ]
-      )
-    end
-
-    test "slides tiles to the right" do
-      assert(
-        Game2048.Grid.move(
-          [
-            [2, :empty, 2],
-            [16, 16, 16],
-            [:empty, 2, 32]
-          ],
-          :right
-        ) == [
-          [:empty, :empty, 4],
-          [:empty, 16, 32],
-          [:empty, 2, 32]
-        ]
-      )
-    end
-
-    test "slides tiles up" do
-      assert(
-        Game2048.Grid.move(
-          [
-            [2, :empty, 2],
-            [16, :obstacle, 16],
-            [:empty, 2, 32]
-          ],
-          :up
-        ) == [
+      grid =
+        [
           [2, :empty, 2],
           [16, :obstacle, 16],
           [:empty, 2, 32]
         ]
-      )
+        |> Game2048.Grid.move(:left)
+        |> reject_elements(tile_that_appears_on_every_move())
+
+      assert grid == [
+               [4, :empty, :empty],
+               [16, :obstacle, 16],
+               [2, 32, :empty]
+             ]
+    end
+
+    test "slides tiles to the right" do
+      grid =
+        [
+          [2, :empty, 2],
+          [16, 16, 16],
+          [:empty, 2, 32]
+        ]
+        |> Game2048.Grid.move(:right)
+        |> reject_elements(tile_that_appears_on_every_move())
+
+      assert grid == [
+               [:empty, :empty, 4],
+               [:empty, 16, 32],
+               [:empty, 2, 32]
+             ]
+    end
+
+    test "slides tiles up" do
+      grid =
+        [
+          [2, :empty, 2],
+          [16, :obstacle, 16],
+          [:empty, 2, 32]
+        ]
+        |> Game2048.Grid.move(:up)
+        |> reject_elements(tile_that_appears_on_every_move())
+
+      assert grid == [
+               [2, :empty, 2],
+               [16, :obstacle, 16],
+               [:empty, 2, 32]
+             ]
     end
 
     test "slides tiles down" do
-      assert(
-        Game2048.Grid.move(
-          [
-            [2, :empty, 2],
-            [16, 16, 16],
-            [:empty, 2, 32]
-          ],
-          :down
-        ) == [
-          [:empty, :empty, 2],
-          [2, 16, 16],
-          [16, 2, 32]
+      grid =
+        [
+          [2, :empty, 2],
+          [16, 16, 16],
+          [:empty, 2, 32]
         ]
-      )
+        |> Game2048.Grid.move(:down)
+        |> reject_elements(tile_that_appears_on_every_move())
+
+      assert grid == [
+               [:empty, :empty, 2],
+               [2, 16, 16],
+               [16, 2, 32]
+             ]
+    end
+
+    test "adds new tile after a slide" do
+      grid =
+        [
+          [2, 4, 8],
+          [16, :obstacle, 32],
+          [64, :empty, 128]
+        ]
+        |> Game2048.Grid.move(:right)
+
+      assert grid == [
+               [2, 4, 8],
+               [16, :obstacle, 32],
+               [tile_that_appears_on_every_move(), 64, 128]
+             ]
+    end
+
+    test "does nothing for illegal move" do
+      grid =
+        [
+          [2, 4, 8],
+          [16, :obstacle, 32],
+          [64, :empty, 128]
+        ]
+        |> Game2048.Grid.move(:up)
+
+      assert grid == [
+               [2, 4, 8],
+               [16, :obstacle, 32],
+               [64, :empty, 128]
+             ]
     end
   end
 end
